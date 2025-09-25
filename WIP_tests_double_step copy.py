416cb18c588ae26b3babfa885586e239253c08b9
CarlosR <carigar4@alumni.uv.es>
2025-09-25 10:14:18 +0200

    WIP

--- WIP_tests_double_step copy.py
+++ WIP_tests_double_step copy.py
@@ -20,8 +20,8 @@ N2 = 100  # Number of epochs for the second step
 # Hyperparams
 p1 = {
     'learning_rate': 0.01, 'hidden_size': 48, 'dropout': {'embedding_attn': 0.3, 'after_attn': 0.225, 'feedforward': 0.225, 'embedding_pos': 0.225},
-    'quantum' = True, 'num_head': 4, 'num_transf': 1, 'mlp_size': 6, 'patch_size': 4, 'weight_decay': 1e-7, 'attention_selection': 'filter', 'entangle': True,
-    'penny_or_kipu': 'kipu', 'RD': 1, 'convolutional': True, 'paralel': 2, 'patience': -1, 'scheduler_factor': 0.999, 'q_stride': 1  # No early stopping
+    'quantum' = True, 'num_head': 4, 'num_transf': 1, 'mlp_size': 6, 'patch_size': 4, 'weight_decay': 1e-7, 'attention_selection': 'none', 'entangle': True,
+    'connectivity': 'david_star', 'RD': 1, 'patience': -1, 'scheduler_factor': 0.999, 'q_stride': 1  # No early stopping
 }
 
 p2 = {
@@ -75,21 +75,50 @@ for idx in range(50):
         learning_rate=p1['learning_rate'], num_epochs=N1, device=device, mapping=False,
         res_folder=str(save_path), hidden_size=p1['hidden_size'], dropout=p1['dropout'],
         num_heads=p1['num_head'], patch_size=p1['patch_size'], num_transf=p1['num_transf'],
-        mlp=p1['mlp_size'], wd=p1['weight_decay'], patience= p1['patience'], scheduler_factor= p1['scheduler_factor']
+        mlp=p1['mlp_size'], wd=p1['weight_decay'], patience= p1['patience'], scheduler_factor= p1['scheduler_factor'], autoencoder=True
     )
 
     print(f"\nAutoencoder training completed succesfully.\nTest MSE (first step): {test_mse:.2f}")
 
     # Create second model for the second step
     model2 = qpctorch.quantum.VisionTransformer(
-        data_type="original", img_size=28, num_channels=3, num_classes=6,
+        img_size=img_size, num_channels=num_channels, num_classes=7,
         patch_size= p2['patch_size'], hidden_size=p2['hidden_size'], num_heads=p2['num_head'],
         num_transformer_blocks=p2['num_transf'], attention_selection=p2['attention_selection'],
         mlp_hidden_size=p2['mlp_size'], dropout=p2['dropout'],
-        channels_last=False, entangle=p2['entangle'], penny_or_kipu=p2['penny_or_kipu'], 
-        RD=p2['RD'], convolutional = p2['convolutional'], paralel=p2['paralel']
+        channels_last=False, entangle=p2['entangle'], q_stride = p2['q_stride'], 
+        RD=p2['RD'], paralel=p2['paralel']
     )
-    print('\nTraining second model for others class...')
+
+    # Prepare datasets for the second step: get latent representations for each dataset and transform them into a new dataloader
+    DataLoaders = [train_dl, val_dl, test_dl]
+    LatentDatasetsTensors = []
+    QuantumLayer = qpctorch.quantum.pennylane_backend.QuantumLayer(num_qubits = p1['mlp_size'], entangle = p1['entangle'], graph = p1['connectivity']) if p1['quantum'] else torch.nn.Identity()
+    for dl in DataLoaders:
+        all_latents = []
+        all_labels = []
+        all_indices = []
+        for images, labels, indices in dl:
+            images = images.to(device)
+            with torch.no_grad():
+                latent_representations = QuantumLayer( model1.get_latent_representation(images) )
+            latent_representations = latent_representations.cpu()
+
+            all_latents.extend( latent_representations )
+            all_labels.extend( labels )
+
+        all_latents = torch.stack(all_latents)
+        all_labels = torch.tensor(all_labels)
+
+
+        LatentDatasetsTensors.append( zip(list(all_latents,all_labels) ) )
+
+    latent_train_dl, latent_val_dl, latent_test_dl = qpctorch.data.create_dataloaders(data_dir = None, batch_size = B, channels_last = channels_last,
+                                        tensors = LatentDatasetTensors, transforms={'train': q_train_transforms, 'val': q_valid_transforms, 'test': q_valid_transforms}
+                                        )
+
+    print('\nTraining second model: classifier ViT on latent representations\n')
+
     # Train second model
     test_auc2, test_acc2, val_auc2, val_acc2, params2 = qpctorch.training.train_and_evaluate(
         model2, train_others_dl, val_others_dl, test_others_dl, num_classes=6,
